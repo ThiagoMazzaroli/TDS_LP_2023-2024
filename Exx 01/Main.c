917379be1a368d7c5d12c25ddeafcfb00f326843
Thiago Mazzaroli <thiagomazzaroli63@gmail.com>
2023-11-22 21:13:07 -0300

    Meu primeiro commit!

---
+++ Exx 01/Main.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float nota1;
+    float nota2;
+    float media;
+    
+    printf("Digite sua primeira nota: ");
+    scanf("%f", &nota1);
+    
+    printf("Digite sua segunda nota: ");
+    scanf("%f", &nota2);
+    
+    media = (nota1 + nota2) / 2;
+    
+    if (media >= 0 && media <= 10)
+{
+        
+    printf("sua média é: %.2f", media);
+    
+} 
+    else
+{
+    printf("media invalida, insira uma nota valida");
+    
+}
+    return 0;
+}
---
+++ Exx 02/Main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    float valorProduto;
+    
+    printf("Digite o valor do seu produto: ");
+    scanf("%f", &valorProduto);
+    
+    if(valorProduto <= 50){
+   
+        float aumento1 = valorProduto * 0.05;
+        float valorFinal1 = valorProduto + aumento1;
+        printf("O valor final dos eu produto ficou: %.2f ", valorFinal1);
+        
+    }
+    
+    else if (valorProduto >= 50 && valorProduto <= 100){
+        
+        float aumento2 = valorProduto * 0.10;
+        float valorFinal2 = valorProduto + aumento2;
+        printf("O valor final dos eu produto ficou: %.2f ", valorFinal2);
+        
+    }
+    
+    else if(valorProduto > 100){
+        
+        float aumento3 = valorProduto * 0.15;
+        float valorFinal3 = valorProduto + aumento3;
+        printf("O valor final dos eu produto ficou: %.2f ", valorFinal3);
+        
+    }
+    
+    
+    
+    
+
+    return 0;
+}
