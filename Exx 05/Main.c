885e71d51fe8f33319a35ce570a2fc5e4b292ff0
Thiago Mazzaroli <thiagomazzaroli63@gmail.com>
2023-11-30 22:15:18 -0300

    New Commit!

---
+++ Exx 05/Main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+  char cores[3];
+
+  cores[0] = 'a';
+  cores[1] = 'b';
+  cores[2] = 'c';
+
+  printf("O valor da posição 0 é: %c", cores[0]);
+
+  printf("Vetores!\n");
+
+  return 0;
+
+
+}
---
+++ Tabuada/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+     
+    int resultados[9];
+    int multi;
+    
+    
+    
+    for(int num = 1; num <= 10; num++)
+    {
+        
+        printf("TABUADA %d\n", num);
+        
+        for(int i= 1; i <= 10; i++){
+        
+        multi = num * i;
+        printf("Tabuada: %d * %d = %d\n", num, i, multi);
+        
+
+        }
+    }
+    
+    
+    
+
+    return 0;
+}
